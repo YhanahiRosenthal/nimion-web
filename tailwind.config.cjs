9c65eb7bca68e64c714a71087b394f84d62c0474
Cristian Lotorto <cristianlotorto@gmail.com>
2023-08-22 18:17:19 -0300

    NW: heaeder visuals finsihed

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,18 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				"primary-dark-background": "#080E29",
+			},
+			spacing: {
+				"25":"100px",
+			},
+			fontFamily: {
+                'montserrat': ['Montserrat'],
+                'lato': ['Lato'],
+                'garamond': ['Garamond']
+            }
+		}
 	}
 }
