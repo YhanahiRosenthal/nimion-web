091db921cfa77cb95b682e9585f5a3c2280a2be7
Cristian Lotorto <cristianlotorto@gmail.com>
2023-08-23 16:11:24 -0300

    NW-8: our mission section vissuals finished

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,16 +5,35 @@ module.exports = {
 		extend: {
 			colors: {
 				"primary-dark-background": "#080E29",
-				"secondary-light": "#8893F3"
+				"secondary-light": "#8893F3",
+				"logo-gray": "#DADFF7",
+				"golden": "#CCA56A",
+				"black-background": "#3c3c3c",
 			},
 			spacing: {
-				"25":"100px",
+				"25": "100px",
+				"88": "22rem",
+				"112": "28rem",
+				"128": "32rem",
+				"144": "36rem",
+				"160": "40rem",
+				"176": "44rem",
+				"192": "48rem",
+				"208": "52rem",
+				"224": "56rem",
+				"228": "57rem",
+				"232": "58rem",
+				"240": "60rem",
+				"256": "64rem",
 			},
 			fontFamily: {
                 'montserrat': ['Montserrat'],
                 'lato': ['Lato'],
                 'garamond': ['Garamond']
-            }
+            },
+			boxShadow: {
+				"cards": "8px 7px 11px -7px rgba(0,0,0,0.75)",
+			}
 		}
 	}
 }
