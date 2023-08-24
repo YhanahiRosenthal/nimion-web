1ad1df13ae0d0dec15b5ce12b1dd427901d9bcbc
Cristian Lotorto <cristianlotorto@gmail.com>
2023-08-23 23:38:19 -0300

    NW-8: tailwind config hotfix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -34,7 +34,28 @@ module.exports = {
             },
 			boxShadow: {
 				"cards": "4px 5px 10px -7px rgba(0,0,0,0.75)",
+				'custom': '0 0 1.6rem #8893F3',
+			},
+			animation: {
+				widther: 'expandWidth 2s',
+				appear: 'appear 2s',
+			},
+			keyframes: {
+				'appear': {
+					'0%': {
+						opacity: '0',
+						transform: 'translateY(30%)'
+					},
+					'100%': {
+						opacity: '1',
+						transform: 'translateY(0)'
+					},
+				},
+				'expandWidth': {
+					'0%': { width: '0' },
+					'100%': { width: '50%' },
+				},
 			}
-		}
 	}
+}
 };
