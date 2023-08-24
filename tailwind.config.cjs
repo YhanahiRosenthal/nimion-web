b409e1dcdfcfce2859b9fc44191d016dd09bd763
YhanahiRosenthal <yhanahi16@gmail.com>
2023-08-23 21:47:00 -0300

    NW-3: transitions added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -18,7 +18,27 @@ module.exports = {
             },
 			boxShadow: {
 				'custom': '0 0 1.6rem #8893F3',
-			  },
+			},
+			animation: {
+				widther: 'expandWidth 2s',
+				appear: 'appear 2s',
+			},
+			keyframes: {
+				'appear': {
+					'0%': { 
+						opacity: '0',
+						transform: 'translateY(30%)'
+					},
+					'100%': { 
+						opacity: '1',
+						transform: 'translateY(0)'
+					},
+				},
+				'expandWidth': {
+					'0%': { width: '0' },
+					'100%': { width: '50%' },
+				},
+			}
 		}
 	}
-}
+};
